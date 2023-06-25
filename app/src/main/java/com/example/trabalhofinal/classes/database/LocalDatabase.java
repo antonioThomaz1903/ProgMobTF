d96a0afbebac9193aea918203e1c1ae852f962bc
Antonio Thomaz <antoniotor987@gmail.com>
2023-06-25 16:24:19 -0400

    Att

--- app/src/main/java/com/example/trabalhofinal/classes/database/LocalDatabase.java
+++ app/src/main/java/com/example/trabalhofinal/classes/database/LocalDatabase.java
@@ -6,16 +6,20 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.trabalhofinal.classes.DAO.ChatDAO;
+import com.example.trabalhofinal.classes.DAO.MensagemDAO;
 import com.example.trabalhofinal.classes.DAO.NotificacaoDAO;
 import com.example.trabalhofinal.classes.DAO.PostDAO;
 import com.example.trabalhofinal.classes.DAO.TagDAO;
 import com.example.trabalhofinal.classes.DAO.UsuarioDAO;
+import com.example.trabalhofinal.classes.entities.Chat;
+import com.example.trabalhofinal.classes.entities.Mensagem;
 import com.example.trabalhofinal.classes.entities.Notificacao;
 import com.example.trabalhofinal.classes.entities.Post;
 import com.example.trabalhofinal.classes.entities.Tag;
 import com.example.trabalhofinal.classes.entities.Usuario;
 
-@Database(entities = {Usuario.class, Post.class, Notificacao.class,Tag.class}, version = 2)
+@Database(entities = {Usuario.class, Post.class, Notificacao.class,Tag.class, Mensagem.class, Chat.class}, version = 3)
 public abstract class LocalDatabase extends RoomDatabase{
 
     private static LocalDatabase INSTANCE;
@@ -31,4 +35,6 @@ public abstract class LocalDatabase extends RoomDatabase{
     public abstract PostDAO postModel();
     public abstract NotificacaoDAO notificacaoModel();
     public abstract TagDAO tagModel();
+    public abstract ChatDAO chatModel();
+    public abstract MensagemDAO mensagemModel();
 }
