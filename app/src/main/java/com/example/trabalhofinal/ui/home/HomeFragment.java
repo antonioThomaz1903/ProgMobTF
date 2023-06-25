115e008025eafe2f231413601fcbfd9a700ce208
Patrick Paredes <patrickparedes44@gmail.com>
2023-06-25 12:47:05 -0400

    tela HomeFragment

--- app/src/main/java/com/example/trabalhofinal/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/trabalhofinal/ui/home/HomeFragment.java
@@ -1,37 +1,47 @@
 package com.example.trabalhofinal.ui.home;
 
+import static android.os.Build.VERSION_CODES.R;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.trabalhofinal.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+import java.awt.font.NumericShaper;
 
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+public class HomeFragment extends AppCompatActivity {
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+
+        ImageButton profileImageButton = findViewById(R.id.profileImageView);
+        profileImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lógica para abrir as configurações do usuário
+                Toast.makeText(HomeFragment.this, "Configurações do usuário", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageButton addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lógica para adicionar um novo item
+                Toast.makeText(HomeFragment.this, "Adicionar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
